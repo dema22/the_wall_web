0f3689431877c52fbd0ec7d4fce3391ee8ab1e28
dema22 <felipedemaria@hotmail.com>
2022-05-19 18:54:30 -0300

    Added the call to get the post of the logged user

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,17 @@ export const Home = () => {
             }
         )();
     }, []);
+
+    const viewUserPosts = async () => {
+        const userId = JSON.parse(localStorage.getItem("userId"));
+        console.log("Viewing my posts");
+        // Send request to the API to view the posts from the logged user.
+        const response = await axios.get('post/profile/' + userId);
+        console.log(response.data);
+    }
+
     return <div>Home
         <h3> Hi {username}!!!</h3>
+        <button onClick={viewUserPosts}>Your posts</button>
     </div>
 }
--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'http://localhost:8000/'
+let refresh = false;
 
 axios.interceptors.response.use(response => response, async error => {
-    if(error.response.status === 401){
+    if(error.response.status === 401 && !refresh){
+        refresh = true;
         console.log("Error 401");
         console.log("Getting refresh token from local storage");
         const refreshToken = localStorage.getItem("refreshToken");
@@ -15,9 +17,11 @@ axios.interceptors.response.use(response => response, async error => {
 
         if(response.status === 200) {
             axios.defaults.headers.common['Authorization'] = `Bearer ${response.data['access']}`;
+            console.log(axios);
             // redo previous requests that failed
             return axios(error.config);
         }
     }
+    refresh = false;
     return error;
 })
