a3413c48a83a3feb4e907566495afeb9de50a892
dema22 <felipedemaria@hotmail.com>
2022-05-26 22:10:23 -0300

    Added error handling to the ProfileComponent

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,12 +2,13 @@ import {useEffect, useState} from 'react';
 import axiosInstance from "../interceptors/axios";
 import TokenService from "../services/TokenService";
 import {CreatePostDialog} from "./CreatePostDialog";
-import {Card} from "@mui/material";
 import {Post} from "./Post";
+import CustomSnackbar from "./CustomSnackbar";
 
 export const Profile = () => {
     const [username,setUsername] = useState('');
     const [userPosts,setUserPosts] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     // Run only on the first render
     useEffect(() => {
@@ -16,54 +17,63 @@ export const Profile = () => {
     }, []);
 
     const getUserInfo = async () => {
-        const response = await axiosInstance.get('profile/user/' + TokenService.getUserId());
-        console.log(response);
-        setUsername(response.data.username);
+        try {
+            // We send request to the API to get the profile information of the logged user.
+            const responseUserInfo = await axiosInstance.get('profile/user/' + TokenService.getUserId());
+            setUsername(responseUserInfo.data.username);
+        }catch(e) {
+            setErrorMessage("We are sorry, something went wrong. Try again later.");
+        }
     }
 
     const getUserPosts = async () => {
-        // We Send request to the API to get all posts from the logged user.
-        const userPostResponse = await axiosInstance.get('post/profile/' + TokenService.getUserId());
-        console.log(userPostResponse.data);
-        setUserPosts(userPostResponse.data);
+        try {
+            // We Send request to the API to get all posts from the logged user.
+            const userPostResponse = await axiosInstance.get('post/profile/' + TokenService.getUserId());
+            setUserPosts(userPostResponse.data);
+        } catch(e) {
+            setErrorMessage("We are sorry, something went wrong. Try again later.");
+        }
     }
 
     // We render Post Components using our state variable (posts).
     const renderUserPosts = () => {
-        if (userPosts.length < 1) {
-            return(
-                <Card> Create a new post !</Card>
-            )
+        if (userPosts.length !== 0) {
+            return userPosts.map((post) => {
+                let formattedDate = post.created_at.split('T');
+                let hoursAndMinutes = formattedDate[1].split(':');
+                return (
+                    <Post
+                        key={post.id}
+                        content={post.content}
+                        title={post.title}
+                        author={post.user_name}
+                        time={`${formattedDate[0]}  ${hoursAndMinutes[0]}:${hoursAndMinutes[1]}`}
+                        showAuthor={false}
+                    >
+                    </Post>
+                )
+            });
         }
-        return userPosts.map( (post) => {
-            let formattedDate = post.created_at.split('T');
-            let hoursAndMinutes= formattedDate[1].split(':');
-            return (
-                <Post
-                    key={post.id}
-                    content={post.content}
-                    title={post.title}
-                    author={post.user_name}
-                    time={`${formattedDate[0]}  ${hoursAndMinutes[0]}:${hoursAndMinutes[1]}`}
-                    showAuthor={false}
-                >
-                </Post>
-            )
-        })
     };
     // Functions that is triggered from the CreateDialogPost component. We receive the new post, and we update our state array with the new post.
     const addNewPost = (newPost) => {
         setUserPosts([newPost,...userPosts]);
     };
 
+    const handleClose = () => {
+        setErrorMessage('');
+    }
+
     return (
         <div style={{textAlign: "center"}}>
             <h2> 👋 {username}!!!</h2>
-            <p> From your profile can view all the post you have created so far or create a new one</p>
+            <p> From your profile you can view all the post you have created so far or create a new one</p>
             <CreatePostDialog addPost={addNewPost}/>
             <>
                 {renderUserPosts()}
             </>
+            {errorMessage && <CustomSnackbar onClose={handleClose} open={true} message={errorMessage} />}
         </div>
     );
 }
